8857106c6c708810371e043d51e5c48fac010714
Ecan ESE <ercan.ese@bilgeadam.com>
2023-05-10 11:41:19 +0300

    add python app

---
+++ Python/app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+
+def hello_world():
+    return '<h2>Hello World</h2>'
+
+
+if __name__ == '__main__':
+    app.run()
